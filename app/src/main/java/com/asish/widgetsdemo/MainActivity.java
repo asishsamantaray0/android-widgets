14edada5939aedc4e604516ed196e31c8ad2c318
Asish Samantaray <samantaray.asish09@gmail.com>
2022-02-06 11:25:43 +0530

    Refactored code for CheckBox widget.

--- app/src/main/java/com/asish/widgetsdemo/MainActivity.java
+++ app/src/main/java/com/asish/widgetsdemo/MainActivity.java
@@ -11,40 +11,48 @@ import java.util.StringJoiner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CheckBox checkBox1;
+    private CheckBox checkBox2;
+    private CheckBox checkBox3;
+
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CheckBox checkBox1 = findViewById(R.id.checkBox);
-        CheckBox checkBox2 = findViewById(R.id.checkBox2);
-        CheckBox checkBox3 = findViewById(R.id.checkBox3);
+        checkBox1 = findViewById(R.id.checkBox);
+        checkBox2 = findViewById(R.id.checkBox2);
+        checkBox3 = findViewById(R.id.checkBox3);
 
         // Button for CheckBox.
         Button btnCheckBox = findViewById(R.id.btn_checkbox);
+        // OnClick Handler for CheckBox Button.
+        btnCheckBox.setOnClickListener((view -> checkBoxButtonHandler()));
 
-        btnCheckBox.setOnClickListener((view -> {
+    }
 
-            // StringJoiner class (Java 8) is used to Join Strings based the delimiter provided in the Constructor.
-            StringJoiner joiner = new StringJoiner(", ");
+    /**
+     * OnClick Handler for CheckBox Button.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void checkBoxButtonHandler() {
+        // StringJoiner class (Java 8) is used to Join Strings based the delimiter provided in the Constructor.
+        StringJoiner joiner = new StringJoiner(", ");
 
-            if (checkBox1.isChecked() || checkBox2.isChecked() || checkBox3.isChecked()) {
-                if (checkBox1.isChecked()) {
-                    joiner.add(checkBox1.getText());
-                }
-                if (checkBox2.isChecked()) {
-                    joiner.add(checkBox2.getText());
-                }
-                if (checkBox3.isChecked()) {
-                    joiner.add(checkBox3.getText());
-                }
-                Toast.makeText(this, "Checked - " + joiner, Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(this, "Please Select any Check Box.", Toast.LENGTH_LONG).show();
+        if (checkBox1.isChecked() || checkBox2.isChecked() || checkBox3.isChecked()) {
+            if (checkBox1.isChecked()) {
+                joiner.add(checkBox1.getText());
             }
-
-        }));
-
+            if (checkBox2.isChecked()) {
+                joiner.add(checkBox2.getText());
+            }
+            if (checkBox3.isChecked()) {
+                joiner.add(checkBox3.getText());
+            }
+            Toast.makeText(this, "Checked - " + joiner, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Please Select any Check Box.", Toast.LENGTH_LONG).show();
+        }
     }
 }
